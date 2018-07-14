a38a98f4790dda6451388d3b84038ca3a729eecd
CGearhart <cgearhart@gmail.com>
2018-07-15 03:06:13 +0530

    update project 3 TimedQueue to buffer last put only
    Signed-off-by: adsarode <adsarode@gmail.com>

--- Projects/1_Sudoku/solution.py
+++ Projects/1_Sudoku/solution.py
@@ -8,7 +8,8 @@ square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','45
 unitlist = row_units + column_units + square_units
 
 # TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+diagonal_units = [[(rows[i] + cols[i]) for i in range(9)], [(rows[i] + cols[8-i]) for i in range(9)]]
+unitlist = unitlist + diagonal_units
 
 
 # Must be called after all units (including diagonals) are added to the unitlist
@@ -54,8 +55,27 @@ def naked_twins(values):
     https://github.com/udacity/artificial-intelligence/blob/master/Projects/1_Sudoku/pseudocode.md
     """
     # TODO: Implement this function!
-    raise NotImplementedError
 
+    # Step 1 : Identify all keys with length 2 (2 possible numbers)
+    values_new = values.copy()
+    possible_twins = [key for key in values_new.keys() if len(values_new[key]) == 2]
+
+    # Step 2 : Identify all twin pairs - pairs in unit which have idential twins
+    verified_twins = []
+    for twin_key in possible_twins:
+        for peer in peers[twin_key]:
+            if set(values_new[twin_key]) == set(values_new[peer]):
+                if not ([twin_key, peer] in verified_twins or [peer, twin_key] in verified_twins): # Avoid duplicate entry, This duplicate entry has higher cost in Step 3
+                    verified_twins.append([twin_key, peer])
+
+    # Step 3 : Remove the digits in identified twins from its peers, except the twin.
+    for twins in verified_twins:
+        twin_peers_intersect = set(peers[twins[0]]) & set(peers[twins[1]])
+        for peer in twin_peers_intersect:
+            for rem_digit in values_new[twins[0]]:
+                values_new = assign_value(values_new, peer, values_new[peer].replace(rem_digit, ''))
+
+    return values_new
 
 def eliminate(values):
     """Apply the eliminate strategy to a Sudoku puzzle
@@ -74,7 +94,15 @@ def eliminate(values):
         The values dictionary with the assigned values eliminated from peers
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    single_values = [key for key in values.keys() if len(values[key]) == 1]
+
+    for key in single_values:
+            digit = values[key]
+            key_peers = peers[key]
+            for peer in key_peers:
+                values[peer] = values[peer].replace(digit, '')
+
+    return values
 
 
 def only_choice(values):
@@ -98,7 +126,20 @@ def only_choice(values):
     You should be able to complete this function by copying your code from the classroom
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for digit in '123456789':
+        for unit in unitlist:
+            counter = 0
+            onlyBox = None
+            for box in unit:
+                if digit in values[box]:
+                    counter = counter+1
+                    if counter > 1:
+                        break
+                    only_box = box
+            if counter is 1:
+                values[only_box] = digit
+
+    return values
 
 
 def reduce_puzzle(values):
@@ -113,10 +154,32 @@ def reduce_puzzle(values):
     -------
     dict or False
         The values dictionary after continued application of the constraint strategies
-        no longer produces any changes, or False if the puzzle is unsolvable 
+        no longer produces any changes, or False if the puzzle is unsolvable
     """
     # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+
+        # Add naked_twins function call
+        values = naked_twins(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+
+    return values
 
 
 def search(values):
@@ -139,7 +202,32 @@ def search(values):
     and extending it to call the naked twins strategy.
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    #"Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False
+    #print("==============================")
+    #display(values)
+
+    # Choose one of the unfilled squares with the fewest possibilities
+    min_len = 9
+    min_len_box = None
+    for box in boxes:
+        if len(values[box]) > 1 and len(values[box]) < min_len:
+            min_len_box = box
+            min_len = len(values[box])
+
+    if min_len_box is None: # Means that all boxes have exactly 1 number in them
+        return values
+
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for val in values[min_len_box]:
+        values_try = values.copy()
+        values_try[min_len_box] = val
+        sudoku_solution =  search(values_try)
+        if sudoku_solution:
+            return sudoku_solution
 
 
 def solve(grid):
@@ -149,7 +237,7 @@ def solve(grid):
     ----------
     grid(string)
         a string representing a sudoku grid.
-        
+
         Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
 
     Returns
